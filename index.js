170cced22dfbdf76f152b1a2b9f4c5e465c12a9f
kamaziki <54082081+kamaziki@users.noreply.github.com>
2019-08-14 00:30:56 +0300

    z
    zzz

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+const CheeseburgerMenu = require('./dist/index');
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+module.exports = CheeseburgerMenu;
